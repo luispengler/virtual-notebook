b75b2449704ef1b67763ad5aca09147d6ac04c5a
Luis Spengler <luispengler@protonmail.com>
2022-03-24 09:52:28 -0400

    Updated some thinga

--- 12th-grade/doc/controlador-de-mesa-de-som/relatorio1/.auctex-auto/relatorio1.el
+++ 12th-grade/doc/controlador-de-mesa-de-som/relatorio1/.auctex-auto/relatorio1.el
@@ -2,7 +2,11 @@
  "relatorio1"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("babel" "portuguese") ("geometry" "margin=3.5cm") ("parskip" "parfill") ("tocbibind" "nottoc") ("biblatex" "backend=biber")))
+                     '(("inputenc" "utf8") ("babel" "portuguese") ("geometry" "margin=2.5cm" "headheight=0pt" "headsep=0pt") ("parskip" "parfill") ("tocbibind" "nottoc") ("biblatex" "backend=biber")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -17,13 +21,15 @@
     "wrapfig"
     "caption"
     "subcaption"
+    "karnaugh-map"
+    "setspace"
     "parskip"
     "tocbibind"
     "biblatex"
     "authblk")
-   (LaTeX-add-labels
-    "fig:E")
+   (TeX-add-symbols
+    "HRule")
    (LaTeX-add-bibliographies
-    "/home/luispengler/drive/LinuxFabrik/Research/read/bib"))
+    "/home/luispengler/drive/NextCloud/Research/read/bib"))
  :latex)
 
