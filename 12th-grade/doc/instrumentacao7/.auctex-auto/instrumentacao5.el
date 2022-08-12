a1340b790881afb46354ee92dbf6dd91c556478a
Luis Spengler <luispengler@protonmail.com>
2022-08-12 12:28:44 -0400

    Updating a lot of stuff

---
+++ 12th-grade/doc/instrumentacao7/.auctex-auto/instrumentacao5.el
@@ -0,0 +1,37 @@
+(TeX-add-style-hook
+ "instrumentacao5"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("babel" "portuguese") ("geometry" "margin=2.5cm") ("parskip" "parfill") ("tocbibind" "nottoc") ("biblatex" "backend=biber")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (TeX-run-style-hooks
+    "latex2e"
+    "ligacao"
+    "article"
+    "art10"
+    "inputenc"
+    "babel"
+    "geometry"
+    "amsmath"
+    "physics"
+    "pgfplots"
+    "titlesec"
+    "graphicx"
+    "wrapfig"
+    "caption"
+    "subcaption"
+    "tabularx"
+    "multirow"
+    "parskip"
+    "tocbibind"
+    "biblatex"
+    "authblk")
+   (TeX-add-symbols
+    "svgwidth")
+   (LaTeX-add-bibliographies
+    "/home/luispengler/drive/LinuxFabrik/Research/read/bib"))
+ :latex)
+
---
+++ 12th-grade/doc/instrumentacao7/.auctex-auto/instrumentacao6.el
@@ -0,0 +1,36 @@
+(TeX-add-style-hook
+ "instrumentacao6"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("babel" "portuguese") ("geometry" "margin=2.5cm") ("parskip" "parfill") ("tocbibind" "nottoc") ("biblatex" "backend=biber")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "ligacao"
+    "article"
+    "art10"
+    "inputenc"
+    "babel"
+    "geometry"
+    "amsmath"
+    "physics"
+    "pgfplots"
+    "titlesec"
+    "graphicx"
+    "wrapfig"
+    "caption"
+    "subcaption"
+    "tabularx"
+    "parskip"
+    "tocbibind"
+    "biblatex"
+    "authblk")
+   (TeX-add-symbols
+    "svgwidth")
+   (LaTeX-add-bibliographies
+    "/home/luispengler/drive/LinuxFabrik/Research/read/bib"))
+ :latex)
+
